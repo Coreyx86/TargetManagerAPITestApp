920805b01c5560b6c4edcd212de4a490a8ab87a2
CoreyNelson <corey@rsi.co>
2019-12-21 15:00:18 -0500

    Adding in gentity_s WIP

--- GhostShtuff/Structures/Definitions.cs
+++ GhostShtuff/Structures/Definitions.cs
@@ -8,6 +8,15 @@ namespace GhostShtuff
 {
     public class Definitions
     {
+        /* gentity_s->flags */
+        public uint FL_GODMODE = 0x1;
+        public uint FL_DEMI_GODMODE = 0x2;
+        public uint FL_NO_KNOCKBACK = 0x8;
+        public uint FL_CAN_RADIUS_DAMAGE = 0x10;
+        public uint FL_SUPPORTS_LINKTO = 0x1000;
+        public uint FL_GRENADE_TOUCH_DAMAGE = 0x4000;
+        public uint FL_STABLE_MISSILES = 0x20000;
+
         public enum sessionState_t
         {
             SESS_STATE_PLAYING,
@@ -21,6 +30,28 @@ namespace GhostShtuff
             CON_DISCONNECTED,
             CON_CONNECTING,
             CON_CONNECTED
+        };
+
+        public enum entityType_t
+        {
+            ET_GENERAL = 0x0,
+            ET_PLAYER = 0x1,
+            ET_PLAYER_CORPSE = 0x2,
+            ET_ITEM = 0x3,
+            ET_MISSILE = 0x4,
+            ET_INVISIBLE = 0x5,
+            ET_SCRIPTMOVER = 0x6,
+            ET_SOUND_BLEND = 0x7,
+            ET_FX = 0x8,
+            ET_LOOP_FX = 0x9,
+            ET_PRIMARY_LIGHT = 0xA,
+            ET_TURRET = 0xB,
+            ET_HELICOPTER = 0xC,
+            ET_PLANE = 0xD,
+            ET_VEHICLE = 0xE,
+            ET_VEHICLE_COLLMAP = 0xF,
+            ET_VEHICLE_CORPSE = 0x10,
+            ET_VEHICLE_SPAWNER = 0x11
         };
     }
 }
---
+++ GhostShtuff/Structures/gentity_s.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostShtuff
+{
+    public class gentity_union
+    {
+        item_ent_t item[2];
+        trigger_ent_t trigger;
+        mover_ent_t mover;
+        corpse_ent_t corpse;
+    }   
+
+    public class gentity_s
+    {
+        //entityState_s s; // 0x0
+        //enitityShared_t r; // 0xFC
+        public gclient_s client { get; set; } // 0x158 //pointer to gclient
+        //turretInfo_s* pTurretInfo; // 0x15C
+        //scr_vehicle_s* scr_vehicle; // 0x160
+        //dxBody* physicsBody; // 0x164
+        short model; // 0x168
+        byte physicsObject; // 0x16A
+        byte takedamage; // 0x16B
+        byte active; // 0x16C
+        byte handler; // 0x16D
+        byte team; // 0x16E
+        byte freeAfterEvent; //0x16F
+        UInt16 code_classname; // 0x172
+        UInt16 classname; // 0x174
+        UInt16 script_linkname; // 0x176
+        UInt16 target; // 0x178
+        UInt16 targetname; // 0x17A
+        int attachIgnoreCollision; // 0x17C
+        int spawnFlags; // 0x180
+        int flags; // 0x184
+        int unlinkAfterEvent; // 0x188
+        int clipMask; // 0x18C
+        int processedFrame; // 0x190
+        //EntHandle parent; // 0x194
+        int nextthink; // 0x198
+        int health; // 0x19C
+        int maxhealth; // 0x1A0
+        int damage; // 0x1A4
+        int count; // 0x1A8
+
+        gentity_union union;
+        //EntHandle projEntity; // 0x20C
+        //EntHandle linkedEntity; // 0x210
+        int tagInfo; // 0x214
+                    
+        //gentity_s* tagChildren; // 0x218 pointer
+        UInt16[] attachModelNames = new UInt16[19]; // 0x21C
+        UInt16[] attachTagNames = new UInt16[19]; // 0x242
+        int useCount; // 0x268
+        int nextFree; // 0x26C
+        int birthtime; // 0x270
+        byte[] unknown = new byte[0xC]; // 0x274
+    }
+}
