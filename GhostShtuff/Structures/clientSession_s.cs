6d921e1a8768cdbcb9e268e388d741c1e4e409c1
CoreyNelson <corey@rsi.co>
2019-12-21 12:41:28 -0500

    Update clientSession_s.cs

--- GhostShtuff/Structures/clientSession_s.cs
+++ GhostShtuff/Structures/clientSession_s.cs
@@ -3,37 +3,205 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace GhostShtuff
 {
     public class clientSession_s
     {
-        Definitions.sessionState_t sessionState; // 0x3190
-        int clientNum; // 0x3194
-        int score; // 0x3198
-        int deaths; // 0x319C
-        int kills; // 0x31A0
-        int assists; // 0x31A4
-        short scriptPersId; // 0x31A8
-        short undefined; // 0x31AA
-        Definitions.clientConnected_t connect; // 0x31AC
-        usercmd_s cmd; // 0x3200
-        usercmd_s oldCmd; // 0x31D8
-        int isLocalClient; // 0x31F8
-        int predictItemPickup; // 0x31FC
-        byte[] name = new byte[32]; // 0x31DC
-        int maxHealth; // 0x3228
-        int enterTime; // 0x322C
-        playerTeamState_t teamState; // 0x3230
-        int voteCount; // 0x3234
-        int teamVoteCount; // 0x3238
-        float moveSpeedScaleMultiplier; // 0x323C
-        int viewmodelIndex; // 0x3240
-        int noSpectate; // 0x3244
-        int teamInfo; // 0x3248
-        clientState_s cs; // 0x324C
-        byte[] padding2 = new byte[16]; // 0x3250
-        int psOffsetTime; // 0x32E8
-        byte[] padding3 = new byte[0x130]; // 0x32EC
+        private uint BASE = 0;
+
+        public Definitions.sessionState_t sessionState
+        {
+            get { return (Definitions.sessionState_t)Manager.Instance.PS3.Extension.ReadUInt32(BASE + 0x0); } //0x0 is the offset in this structure in which this data is positioned 0x3190 is the position of this structure in gclient
+        }// 0x3190
+
+        public int clientNum
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0x4); }
+        } // 0x3194
+
+        public int score
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0x8); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0x8, value); }
+        } // 0x3198
+
+        public int deaths
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0xC); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0xC, value); }
+        } // 0x319C
+
+        public int kills
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0x10); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0x10, value); }
+        } // 0x31A0
+
+        public int assists
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0x14); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0x14, value); }
+
+        } // 0x31A4
+
+        public short scriptPersId
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt16(BASE + 0x18); }
+            set { Manager.Instance.PS3.Extension.WriteInt16(BASE + 0x18, value); }
+
+        } // 0x31A8
+
+        public short undefined
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt16(BASE + 0x1A); }
+        } // 0x31AA
+
+        public Definitions.clientConnected_t connect
+        {
+            get { return (Definitions.clientConnected_t)Manager.Instance.PS3.Extension.ReadUInt32(BASE + 0x1C); }
+            set { Manager.Instance.PS3.Extension.WriteUInt32(BASE + 0x1C, Convert.ToUInt32(value)); }
+
+        } // 0x31AC
+
+        private usercmd_s _cmd = null;
+
+        public usercmd_s cmd
+        {
+            get { return _cmd; }
+            set { _cmd = value; }
+        } // 0x3200
+
+        private usercmd_s _oldCmd = null;
+
+        public usercmd_s oldCmd
+        {
+            get { return _oldCmd; }
+            set { _oldCmd = value; }
+        } // 0x31D8
+
+        public int isLocalClient
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0x70); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0x70, value); }
+        }// 0x31F8
+
+        public int predictItemPickup
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0x74); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0x74, value); }
+
+        } // 0x31FC
+
+        public string name
+        {
+            get { return Manager.Instance.PS3.Extension.ReadString(BASE + 0x78); }
+            set
+            {
+                string tmp = value;
+
+                if (tmp.Length < 32)
+                {
+                    tmp = tmp.Substring(0, 32);
+                }
+
+                Manager.Instance.PS3.Extension.WriteString(BASE + 0x78, tmp); 
+            }
+        } // 0x31DC
+
+        public int maxHealth
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0xAA); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0xAA, value); }
+
+        } // 0x3228
+
+        public int enterTime
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0xAE); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0xAE, value); }
+
+        }// 0x322C
+
+        private playerTeamState_t _teamState = null; //0xB2
+
+        public playerTeamState_t teamState
+        {
+            get { return _teamState; }
+            set { _teamState = value; }
+        }// 0x3230
+
+        public int voteCount
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0xB6); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0xB6, value); }
+
+        } // 0x3234
+
+        public int teamVoteCount
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0xBA); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0xBA, value); }
+        } // 0x3238
+
+        public float moveSpeedScaleMultiplier
+        {
+            get { return Manager.Instance.PS3.Extension.ReadFloat(BASE + 0xBE); }
+            set { Manager.Instance.PS3.Extension.WriteFloat(BASE + 0xBE, value); }
+        } // 0x323C
+
+        public int viewmodelIndex
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0xC2); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0xC2, value); }
+        } // 0x3240
+
+        public int noSpectate
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0xC6); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0xC6, value); }
+        } // 0x3244
+
+        public int teamInfo
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0xCA); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0xCA, value); }
+        } // 0x3248
+
+        private clientState_s _cs = null;
+
+        public clientState_s cs 
+        {
+            get { return _cs; }
+            set { _cs = value; }
+        } // 0x324C
+
+        public byte[] padding2
+        {
+            get { return Manager.Instance.PS3.Extension.ReadBytes(BASE + 0x15A, 16); }
+        }// 0x3250
+
+        public int psOffsetTime
+        {
+            get { return Manager.Instance.PS3.Extension.ReadInt32(BASE + 0x170); }
+            set { Manager.Instance.PS3.Extension.WriteInt32(BASE + 0x170, value); }
+        } // 0x32E8
+
+        public byte[] padding3
+        {
+            get { return Manager.Instance.PS3.Extension.ReadBytes(BASE + 0x174, 0x130); }
+        }// 0x32EC
+
+        public clientSession_s() { }
+
+        public clientSession_s(uint BASE)
+        {
+            this.BASE = BASE;
+            this._cmd = new usercmd_s();
+            this._cmd = new usercmd_s();
+            this._teamState = new playerTeamState_t();
+            this._cs = new clientState_s();
+        }
     }
 }
